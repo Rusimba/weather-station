f03c548df1520c23d586e38b38843ba4aed483fe
Rusiamba <vyav_yav@mail.ru>
2026-09-24 21:14:33 +0300

    I2C for BME280

--- main/weather-station.c
+++ main/weather-station.c
@@ -6,11 +6,16 @@
 #include "esp_log.h"
 #include "wifi.h"
 #include "mqtt.h"
+#include "driver/i2c_master.h"
+
+
 static const char *TAG = "weather";
 typedef struct {
     float temperature;
     float humidity;
 }measurement_t;
+
+
 static QueueHandle_t s_queue_desc;
 void sampler_task(void *pvParameters){
      while (1)
@@ -50,6 +55,40 @@ void publisher_task(void *pvParameters){
 }
 void app_main(void)
 {
+    i2c_master_bus_config_t bus_config =   {
+    .i2c_port = -1,
+    .sda_io_num = GPIO_NUM_21,
+    .scl_io_num = GPIO_NUM_22,
+    .clk_source = I2C_CLK_SRC_DEFAULT,
+    .glitch_ignore_cnt = 7,
+    .flags.enable_internal_pullup = true,
+    };
+    i2c_device_config_t dev_config = {
+    .dev_addr_length = I2C_ADDR_BIT_LEN_7,
+    .device_address = 0x76,
+    .scl_speed_hz = 100000,
+    };
+    i2c_master_bus_handle_t bus_handle;
+    ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &bus_handle));
+    uint8_t address = 0x08;
+    while (address <= 0x77){
+        esp_err_t  res = i2c_master_probe( bus_handle,  address,  50);
+        if (res==ESP_OK){
+            ESP_LOGI(TAG,"Found address: %02x ",address);
+        }
+        address++;
+    }
+    i2c_master_dev_handle_t dev_handle;
+    uint8_t reg = 0xD0;
+    uint8_t chip_id;
+    i2c_master_bus_add_device( bus_handle,&dev_config, &dev_handle);
+    esp_err_t  res = i2c_master_transmit_receive(dev_handle, &reg, 1, &chip_id, 1, 100);
+    if (res==ESP_OK){
+            ESP_LOGI(TAG,"REG =  %02x ",chip_id);
+        }
+    else{
+        ESP_LOGI(TAG,"ERROR =  %02x ",res);
+    }
     wifi_init_sta();
     mqtt_init_publisher();
     s_queue_desc = xQueueCreate(30, sizeof(measurement_t));
